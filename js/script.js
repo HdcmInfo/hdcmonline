0be8e37e567d323eae2cb91b4df42a7df78856fe
HdcmInfo <61444163+HdcmInfo@users.noreply.github.com>
2020-04-11 11:19:47 +1000

    Batch update

--- js/script.js
+++ js/script.js
@@ -4,30 +4,30 @@ function submitToAPI(e) {
        var URL = "https://c39rsx5v9b.execute-api.ap-southeast-2.amazonaws.com/Prod/contactus";
 
             var Namere = /[A-Za-z]{1}[A-Za-z]/;
-            if (!Namere.test($("#name-input").val())) {
-                         alert ("Name can not less than 2 char");
+            if (!Namere.test($("#field1").val())) {
+                         Swal.fire ("Name can not less than 2 char");
                 return;
             }
-            var mobilere = /[0-9]{10}/;
-            if (!mobilere.test($("#phone-input").val())) {
-                alert ("Please enter valid mobile number");
-                return;
-            }
-            if ($("#email-input").val()=="") {
-                alert ("Please enter your email id");
+         //   var mobilere = /[0-9]{10}/;
+        //    if (!mobilere.test($("#field3").val())) {
+        //        alert ("Please enter valid mobile number");
+        //       return;
+         // }
+            if ($("#field2").val()=="") {
+                Swal.fire ("Please enter your email id");
                 return;
             }
 
             var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-            if (!reeamil.test($("#email-input").val())) {
-                alert ("Please enter valid email address");
+            if (!reeamil.test($("#field2").val())) {
+                Swal.fire ("Please enter valid email address");
                 return;
             }
 
-       var name = $("#name-input").val();
-       var phone = $("#phone-input").val();
-       var email = $("#email-input").val();
-       var desc = $("#description-input").val();
+       var name = $("#field1").val();
+       var phone = $("#field3").val();
+       var email = $("#field2").val();
+       var desc = $("#field4").val();
        var data = {
           name : name,
           phone : phone,
@@ -46,12 +46,17 @@ function submitToAPI(e) {
          
          success: function () {
            // clear form and show a success message
-           alert("Successfull");
+           //alert("Thanks for your enquiry, we will get back to you shortly");
+           Swal.fire(
+  'Thank you!',
+  'Thanks for your enquiry, we will get back to you shortly',
+  'success'
+)
            document.getElementById("contact-form").reset();
        location.reload();
          },
          error: function () {
            // show an error message
-           alert("UnSuccessfull");
+           alert("There is some technical issue while sending the message . Please send the email directly to info.hdcm@hdcmonline.org with your query");
          }});
      }
